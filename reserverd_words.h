3db630e8ed9e27da375d2337c92a5b7c7c93add2
Alik <alik2098@gmail.com>
2022-03-07 06:34:05 -0800

    feat: reserved

---
+++ reserverd_words.h
@@ -0,0 +1,11 @@
+/*
+ * a file holding reserverd words and command names
+ */
+
+extern const int LEN_COMMANDS;
+
+extern const char *COMMANDS[];
+
+extern const int LEN_INSTRUCTIONS;
+
+extern const char *INSTRUCTIONS[];
